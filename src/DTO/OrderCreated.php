a1a31c4620b8d61388821acff1fa57f0787b4578
bonbon <andrew.starkov@mindgeekconsultants.com>
2026-03-31 22:30:52 +0200

    order id added to order created event

--- src/DTO/OrderCreated.php
+++ src/DTO/OrderCreated.php
@@ -11,6 +11,8 @@ final readonly class OrderCreated
     public function __construct(
         #[Assert\Uuid]
         public string $productID,
+        #[Assert\Uuid]
+        public string $orderID,
         #[Assert\GreaterThan(0)]
         public int    $quantity,
     ) {}
